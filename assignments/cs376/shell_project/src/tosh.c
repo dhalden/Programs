09081ac469484c4775e348534cb06e3274d264d8
dhalden <dhalden@gmail.com>
2013-10-22 22:16:57 -0400

    "Update to tosh"

--- assignments/cs376/shell_project/src/tosh.c
+++ assignments/cs376/shell_project/src/tosh.c
@@ -32,44 +32,51 @@ struct cmdin * parse(char *cmd)
 int main()
 {
     char cwd[2097152];
-    if(getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        fprintf(stdout, "Current working dir: %s\n", cwd);
-       
-    }
-    else
-        printf("getcwd() error");
+    getcwd(cwd, sizeof(cwd));
 
     char input[2097152]; 
     char *exit  = "exit\n";
+    char *cd = "cd";
     //Command to take input
     printf("tosh$ ");
     fgets(input, sizeof input, stdin);    
     while(strcmp(input, exit))
     {
-    //Command to parse input
-    struct cmdin *cmd;
-    cmd = parse(input);
-    if(strcmp(cmd->cmd, "cd"))
-     {
-      cwd = chdir(cmd->args); 
-     }
-    //command to run input
-    int child_pid();
-
-    printf("\ncmd: %s", cmd->cmd);
-    printf("\nargs: %s\n", cmd->args); 
-    //child_pid = fork();
-
-    //if(child_pid == 0)
-    //{
-    //    execvp(command, arg_extensions);
-    //}
-
-    //Command to take input
-    printf("tosh$ ");
-    free(cmd);
-    fgets(input, sizeof input, stdin);
+        //Command to parse input
+        struct cmdin *cmd;
+        cmd = parse(input);
+        
+        //Change directories
+        if(!strcmp(cd, cmd->cmd))
+        {
+            chdir(cmd->args); 
+            getcwd(cwd, sizeof(cwd));
+        }
+        else if(!strcmp(input, exit))
+        {
+            //command to run input
+            //int child_pid;
+            //int curr_pid;
+            printf("\ncmd: >%s<", cmd->cmd);
+            printf("\nargs: >%s<\n", cmd->args); 
+            
+            //child_pid = fork();
+            //curr_pid = getpid(); 
+            /*if(child_pid == 0)
+            {
+                execvp(input, NULL);
+            }
+            else
+            {
+                wait(child_pid);
+            }
+            //Command to take input
+            */
+        }
+        printf("tosh$ ");
+        free(cmd);
+        fgets(input, sizeof input, stdin);
+        
     }
     return 0;
 }

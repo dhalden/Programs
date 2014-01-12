889803c65b0b50d4d93865ddf26d4a586639363d
dhalden <dhalden@gmail.com>
2014-01-11 21:38:37 -0700

    dis project

---
+++ C/dish.c
@@ -0,0 +1,26 @@
+/*
+ *    Just the basics. For this improved version, I am going to make sure that
+ *    I'm breaking everything down into basic components.
+ *
+ *    Author: D. Halden
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fnctl.h>
+#define KB 1024
+#define MAX_CLEN 2097152
+#define EXITS "exit"
+#define CD "cd"
+
+int main()
+{
+    char input[MAX_CLEN];
+    
+}

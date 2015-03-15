c8dc93b69db8b2466bbd0a8ce6d2ec336284f19a
Derek Halden <dhalden@gmail.com>
2015-03-15 17:12:31 -0400

    cleanup

--- Python/par.py
+++
@@ -1,7 +0,0 @@
-a = pow(2,1001)
-s = str(a)
-p = 0
-for i in range(len(s)):
-    p += int(s[i]) 
-print p
-print s
--- Python/sumosqs.py
+++
@@ -1,8 +0,0 @@
-temp = 0
-for i in range(101):
-    temp += i*i
-
-temp2= (100*(101)/2)
-temp2 = temp2*temp2
-
-print temp2 - temp
